186a309391b0877669a112b732a8405204432575
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2023-12-25 14:24:40 -0800

    Add button and event handler
    When increment button is clicked, the count is incremented once and shown on the page

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 //document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el")
+console.log(countEl)
 
-let count = 10
+let count = 0
 
-console.log(count)
+// listen for clicks on increment button 
+// increment count when clicked
+// change count-el in HTML to show new count
+function increment() 
+{
+    count += 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+
+
+
 
